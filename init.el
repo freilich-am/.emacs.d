e8027605b4c35052cb9768f8d8bac3d26f8b749c
A Freilich <freilich.am@gmail.com>
2019-06-17 20:44:29 -0400

    add stuff to set $PATH variables properly for git flow

--- init.el
+++ init.el
@@ -26,3 +26,17 @@
 (defalias 'list-buffers 'ibuffer)
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell ag magit-gitflow zenburn-theme use-package try org-bullets neotree melpa-upstream-visit magit helm-spotify heap haskell-mode auto-complete auctex alect-themes))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
